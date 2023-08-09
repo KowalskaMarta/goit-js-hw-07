7593e61ff696073235905637175bf73b24787cac
Kowalska Marta <kamarta0991@gmail.com>
2023-08-09 23:45:50 +0200

    Add changes init

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryList =
+document.querySelector("ul.gallery");
+galleryList.insertAdjacentHTML("beforeend", createGallery(galleryItems));
+
+function createGallery(array) {
+    let myGallery = " ";
+    array.forEach(element => {
+        myGallery += `<div class= "gallery__item">
+        <a class="gallery__link" href="${element.orginal}">
+        <img src="${element.preview}"
+        data-source="${element.orginal}" alt="${element.description}" class="gallery__image" /></a></div>`; 
+    });
+}
+return myGallery;
+
 console.log(galleryItems);
